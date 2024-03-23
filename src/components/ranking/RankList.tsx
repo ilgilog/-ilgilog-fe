9ce92aa04e962cd50b20fd11c3ba215806d2c32c
devwoodie <ehddl453@naver.com>
2024-03-23 14:29:09 +0900

    feat: 랭킹화면 구현-2 완료

--- src/components/ranking/RankList.tsx
+++ src/components/ranking/RankList.tsx
@@ -1,52 +1,67 @@
 import { MiniHome } from "components/minime/MiniHome"
 
 export const RankList = () => {
-    return(
-        <ul className="flex flex-wrap mb-10">
-            <li className="">
-                <div className=" bg-white py-5 rounded-xl">
-                    <div className="flex justify-start items-center px-5 pb-3 border-b-[1px] border-solid border-gray-400">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-10 h-10">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>
-                        <span className="text-3xl block ml-3">ehddn453</span>
-                    </div>
-                    
-                    <MiniHome
-                        width={550}
-                        height={350}
-                        boxShadow={"none"}
-                        borderRadius={0}
-                    />
 
-                    <div className="px-5 pt-5 border-t-[1px] border-solid border-gray-400">
-                        <span className="flex justify-between items-center w-1/2 mb-1">
-                            <input type="checkbox" id="favorite" name="favorite-checkbox" value="favorite-button" className="favorite-input" />
-                            <label htmlFor="favorite" className="container favorite-label">
-                                <div className="flex justify-start items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
-                                    <span className="text-3xl ml-2">38 개</span>
-                                    <div className="action ml-3">
-                                        <span className="option-1 text-3xl">좋아요</span>
-                                        <span className="option-2 text-3xl">좋아요 취소</span>
+    const test = [1, 2, 3, 4, 5]
+
+    return(
+        <ul className="grid grid-cols-3 gap-20 my-10 ml-[-34px] w-[1280px] absolute left-[50%] translate-x-[-50%]">
+            {test?.map((item, key) => (
+                <li className="scale-75 origin-top w-[450px] m-auto">
+                    <div className="w-full bg-white py-5 rounded-xl relative">
+                        <div className="flex justify-start items-center px-5 pb-3 border-b-[1px] border-solid border-gray-400">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-10 h-10">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                            <span className="text-3xl block ml-3">ehddn453</span>
+                        </div>
+                        
+                        <MiniHome
+                            width={450}
+                            height={300}
+                            boxShadow={"none"}
+                            borderRadius={0}
+                        />
+
+                        <div className="px-5 pt-5 border-t-[1px] border-solid border-gray-400">
+                            <span className="flex justify-between items-center w-1/2 mb-1">
+                                <input type="checkbox" id="favorite" name="favorite-checkbox" value="favorite-button" className="favorite-input" />
+                                <label htmlFor="favorite" className="container favorite-label">
+                                    <div className="flex justify-start items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                                        <span className="text-3xl ml-2">38 개</span>
+                                        <div className="action ml-3">
+                                            <span className="option-1 text-3xl">좋아요</span>
+                                            <span className="option-2 text-3xl">좋아요 취소</span>
+                                        </div>
                                     </div>
-                                </div>
-                            </label>
-                        </span>
-                        <span className="flex justify-between items-center">
-                            <input type="checkbox" id="favorite" name="favorite-checkbox" value="favorite-button" className="favorite-input" />
-                            <label htmlFor="favorite" className="container favorite-label">
-                                <div className="flex justify-start items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-                                    </svg>
-                                    <span className="text-3xl ml-2">12,000 포인트 사용</span>
-                                </div>
-                            </label>
-                        </span>
+                                </label>
+                            </span>
+                            <span className="flex justify-between items-center">
+                                <input type="checkbox" id="favorite" name="favorite-checkbox" value="favorite-button" className="favorite-input" />
+                                <label htmlFor="favorite" className="container favorite-label">
+                                    <div className="flex justify-start items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+                                        </svg>
+                                        <span className="text-3xl ml-2">12,000 포인트 사용</span>
+                                    </div>
+                                </label>
+                            </span>
+                        </div>
+
+                        <div className="absolute right-[-13%] top-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill={
+                                key === 0 ? "#fdd33e" : key === 1 ? "#c0c0c0" : key === 2 ? "#d87d3d" : "#d5bdaf"
+                                } viewBox="0 0 24 24" strokeWidth={.3} stroke="#000" className="w-24 h-24">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6Z" />
+                            </svg>
+                            <span className="text-3xl text-[#000] absolute right-9 top-8 transform rotate-[46deg]">{key+1}등</span>
+                        </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            ))}
 
             <style>
                 {`
--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -5,7 +5,16 @@ export const Ranking = () => {
     return(
         <div>
             <h2 className="flex justify-center text-3xl mt-3 mb-2">미니홈 랭킹</h2>
-            <RankToggle />
+            <div className="flex justify-between items-center">
+                <div className="flex justify-start items-center cursor-pointer ml-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                    </svg>
+                    <span className="text-xl ml-2">새로고침</span>
+                </div>
+                <RankToggle />
+            </div>
+            
             <RankList />
         </div>
     )
